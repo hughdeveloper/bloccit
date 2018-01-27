3c695c8c766995d45adfdb303e6bfa9a14d43805
Hugh Bethell <hughdeveloper@gmail.com>
2018-01-27 15:26:23 -0800

    checkpoint 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  # the root states the landing page or landing url. THe page we are taken to. It declares the deault page
+  # What is the pound symbol in the middle of welcome#index for??????????????????????
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
