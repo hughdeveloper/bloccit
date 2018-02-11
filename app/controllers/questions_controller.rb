b60a0dbe1c298ca427460839e88c704f353219ae
Hugh Bethell <hughdeveloper@gmail.com>
2018-02-10 19:58:19 -0800

    Checkpoint 10 assignment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   def index
-    @question = Question.all
+    @questions = Question.all
   end
 
   def show
@@ -15,7 +15,7 @@ class QuestionsController < ApplicationController
     @question = Question.new
     @question.title = params[:question][:title]
     @question.body = params[:question][:body]
-    @question.resolved = params[:question][:resolved]
+    @question.resolved = false
 
     if @question.save
       flash[:notice] = "Question was saved"
@@ -28,6 +28,49 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-
+    @question = Question.find(params[:id])
   end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try agin."
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+
+    else
+      flash.now[:alert] = "There was an error deleting the question"
+      render :show
+    end
+  end
+
+  def resolved
+    @question = Question.find(params[:id])
+
+    @question.resolved = params[:question][:resolved]
+
+    if @question.resolved === true
+      @question.resolved = false
+    else
+      @question.resolved = true
+    end
+  end
+
+
+
 end
