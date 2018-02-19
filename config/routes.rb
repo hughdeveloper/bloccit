1c85fb5669271cd2173122d37d08ebf68b9e6e64
Hugh Bethell <hughdeveloper@gmail.com>
2018-02-18 18:40:13 -0800

    checkpoint 14 assignment update

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :advertisements
 
   resources :users, only: [:new, :create]
-
+  post 'users/confirm' => "users#confirm"
 
   #we call the resources method and pass it a Symbol. This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post
 
