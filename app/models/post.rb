65fba567b3eb25675b1a7312d1992d40c1122ed0
Hugh Bethell <hughdeveloper@gmail.com>
2018-04-03 17:45:31 -0700

    checkpoint 20 assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   has_many :favorites, dependent: :destroy
 
   after_create :create_vote
-
+  after_create :create_favorite
   #order posts in descending order by the created time
   default_scope { order('rank DESC') }
   #we title how we are going to call the aciton and then tell it what to do
@@ -38,6 +38,11 @@ class Post < ApplicationRecord
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
+   end
+
+   def create_favorite
+     Favorite.create(post: self, user: self.user)
+     FavoriteMailer.new_post(self).deliver_now
    end
 
    private
